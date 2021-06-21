71cd66144f5f74211e69b422fb8c450b45a6dd43
Glacier <2236022024@qq.com>
2021-06-21 09:20:34 +0800

    第一次提交测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: () => import('../views/Index.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/LoginRegister.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import('../views/Test.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: '/404',
+    component: () => import('../views/404.vue')
   }
 ]
 
@@ -23,3 +29,14 @@ const router = createRouter({
 })
 
 export default router
+
+/*
+知识点：
+
+1、404页面，
+
+以前：path: '*',  这样配置即可将所有不存在的路由都跳转到该页面，
+
+现在：path: '/:catchAll(.*)',   需要这样配置才可以将不存在的路由都跳转到该页面   catchAll(【正则】)
+
+*/
---
+++ src/views/Test.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-form :model="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="密码" prop="pass">
+            <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+            <el-input v-model.number="ruleForm.age"></el-input>
+        </el-form-item>
+    </el-form>
+    <el-input v-model="input"></el-input>
+</template>
+<!-- 
+
+    问题：为何el-form内的输入框无法输入内容
+    解决：将el-form标签的status-icon ref="ruleForm"同时去掉
+ -->
+<script>
+    import {
+        ref
+    } from "vue"
+    export default {
+        setup() {
+            const ruleForm = ref({
+                pass: '',
+                checkPass: '',
+                age: ''
+            })
+            const input = ref('')
+            return {
+                ruleForm,
+                input
+            };
+        }
+    }
+</script>
+
+<style>
+
+</style>
